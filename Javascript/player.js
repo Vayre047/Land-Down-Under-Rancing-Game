aafcc6385884f86b38bc9e93bd69fa4f0afc3fe9
Vayre047 <94454907+Vayre047@users.noreply.github.com>
2023-07-21 17:20:44 +0100

    Intitial Commit

---
+++ Javascript/player.js
@@ -0,0 +1,10 @@
+class Player{
+    constructor(name, gameScreen, record){
+        this.name = name;
+        this.gameScreen = gameScreen;
+        this.score = 0;
+        this.record = record;
+    }
+
+    
+}
