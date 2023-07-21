981c2cea38674fa1cf44517d91e0ae22083ad0ab
Vayre047 <94454907+Vayre047@users.noreply.github.com>
2023-07-21 17:54:17 +0100

    Some changes

--- Javascript/player.js
+++ Javascript/player.js
@@ -1,10 +1,23 @@
 class Player{
-    constructor(name, gameScreen, record){
+    constructor(name, gameScreen, left, top, width, height, imgSrc){
         this.name = name;
         this.gameScreen = gameScreen;
+        this.left = left;
+        this.top = top;
+        this.width = width;
+        this.height = height;
+        this.imgSrc = imgSrc;
         this.score = 0;
-        this.record = record;
-    }
+        this.record = 0;
 
-    
+        this.element = document.createElement("img");
+        this.element.src = imgSrc;
+        this.element.style.position = "absolute";
+        this.element.style.left = `${this.left}px`;
+        this.element.style.top = `${this.top}`;
+        this.element.style.width = `${this.width}px`;
+        this.element.style.height = `${this.height}px`;
+
+        this.gameScreen.appendChild(this.element);
+    }
 }
