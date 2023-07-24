2daf7334a0d5d62f88e5beda2ab091fffffefa2e
Agnieszka Morawska <agnesmora.art@gmail.com>
2023-07-24 11:17:39 +0100

    html + script

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,67 +1,55 @@
 //When clicking startButton, create a new instance of the Game class
 // & start the game by invoking the start() method:
 
-
 window.onload = function () {
-    const startButton = document.getElementById("start-button");
-<<<<<<< HEAD
-    const restartButton = document.getElementById("restart-button");   // dull text?
-    let game;
-=======
-    const restartButton = document.getElementById("restart-button");
->>>>>>> c6006244117ab14bdef957db9cb99c408a6c90c3
+  const startButton = document.getElementById("start-button");
 
-    startButton.addEventListener("click", function () {
-        startGame();
-    });
+  const restartButton = document.getElementById("restart-button");
 
-    let game;
+  startButton.addEventListener("click", function () {
+    startGame();
+  });
 
-    function startGame(){
-        console.log("start game");
-        game = new Game();
+  let game;
 
-        game.start();
-    }
+  function startGame() {
+    console.log("start game");
+    game = new Game();
 
-<<<<<<< HEAD
+    game.start();
+  }
 
-// Function that handles keydown events
-    function handleKeydown(event) {          //dull text? 
-        const key = event.key;
-        const possibleKeystrokes = [
-          "ArrowLeft",
-          "ArrowUp",
-          "ArrowRight",
-          "ArrowDown",
-        ];
+  // Function that handles keydown events
+  function handleKeydown(event) {
+    //dull text?
+    const key = event.key;
+    const possibleKeystrokes = [
+      "ArrowLeft",
+      "ArrowUp",
+      "ArrowRight",
+      "ArrowDown",
+    ];
 
-
-         // Check if the pressed key is in the possibleKeystrokes array
+    // Check if the pressed key is in the possibleKeystrokes array
     if (possibleKeystrokes.includes(key)) {
-        event.preventDefault();
-  
-        // Update player's directionX and directionY based on the key pressed
-        switch (key) {
-          case "ArrowLeft":
-            game.player.directionX = -1;
-            break;
-          case "ArrowUp":
-            game.player.directionY = -1;
-            break;
-          case "ArrowRight":
-            game.player.directionX = 1;
-            break;
-          case "ArrowDown":
-            game.player.directionY = 1;
-            break;
-        }
+      event.preventDefault();
+
+      // Update player's directionX and directionY based on the key pressed
+      switch (key) {
+        case "ArrowLeft":
+          game.player.directionX = -1;
+          break;
+        case "ArrowUp":
+          game.player.directionY = -1;
+          break;
+        case "ArrowRight":
+          game.player.directionX = 1;
+          break;
+        case "ArrowDown":
+          game.player.directionY = 1;
+          break;
       }
-      window.addEventListener("keydown", handleKeydown);
-    };
-        
-
-
-=======
->>>>>>> c6006244117ab14bdef957db9cb99c408a6c90c3
+    }
+    window.addEventListener("keydown", handleKeydown);
+  }
 };
